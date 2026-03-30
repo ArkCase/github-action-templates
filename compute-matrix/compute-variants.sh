64c030c1ded656286b43992142a61b9fb535b72a
Diego Rivera <diego.rivera@armedia.com>
2026-03-30 16:22:00 -0600

    Finalize compute-matrix

---
+++ compute-matrix/compute-variants.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+#
+set -euo pipefail
+
+CANDIDATE_VARIANTS=( "main" )
+
+#
+# See if this container build cares whether there's FIPS
+# anywhere or not ...
+#
+grep -qE "^ARG\s+FIPS(=.*)?\s*$" "${WORK_DIR}/Dockerfile" && CANDIDATE_VARIANTS+=( "fips" )
+
+# TODO: Compute other variants?
+
+#
+# Make sure we have no duplicates
+#
+readarray -t VARIANTS < <(
+	(
+		echo "main"
+		for V in "${CANDIDATE_VARIANTS[@],,}" ; do
+			# Quick trim
+			V="$(echo ${V})"
+			[ -n "${V}" ] || continue
+			[ "${V}" != "all" ] || continue
+			[[ "${V}" =~ ^[^[:space:]]+$ ]] || continue
+			echo "${V}"
+		done
+	) | sort -u | sed -e '/^\s*$/d'
+)
+
+# This should yield the required output
+echo "value=[${VARIANTS[@]@Q}]" | tr ' ' ',' | tr "'" '"'
+exit 0
