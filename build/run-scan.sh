7b0ed7507fd86407d6a76d162378d73153112459
Diego Rivera <diego.rivera@armedia.com>
2026-03-30 19:44:32 -0600

    More progress?

--- build/run-scan.sh
+++ build/run-scan.sh
@@ -3,17 +3,17 @@
 #
 set -euo pipefail
 
-echo "Launching the ${SCAN_TYPE^^} Scan for ${CONTAINER_IMAGE}..."
+echo "Launching the ${SCAN_TYPE^^} Scan for ${AUTHORITATIVE_TAG}..."
 DOCKER_SOCKET="/var/run/docker.sock"
 CMD=(
 	docker run
 		--rm
 		--name "${SCAN_TYPE}-scanner"
-		--env RESULTS_NAME="${SCAN_TYPE}${RESULTS_SUFFIX}"
+		--env RESULTS_NAME="${SCAN_TYPE}${ARTIFACTS_IDENTIFIER}"
 		--volume "${DOCKER_SOCKET}:${DOCKER_SOCKET}"
 		--volume "${SCAN_DIR}:/results"
 		"${SCANNER_IMAGE}"
-		"${SCAN_TYPE}" "${CONTAINER_IMAGE}"
+		"${SCAN_TYPE}" "${AUTHORITATIVE_TAG}"
 )
 
 # Run the command!
