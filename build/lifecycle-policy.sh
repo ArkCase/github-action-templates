1dc2fa70f1ced02a2a10e6e003f4ca60a963b438
Diego Rivera <diego.rivera@armedia.com>
2026-04-01 19:40:14 -0600

    Fix permissions and lifecycle policy rendering

--- build/lifecycle-policy.sh
+++ build/lifecycle-policy.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 . "${GITHUB_ACTION_PATH}/common.sh"
 
-DEFAULT_SNAPSHOT_KEEP_DAYS="30"
-DEFAULT_DEVEL_KEEP_DAYS="30"
-
-JSON="$(mktemp --tmpdir="${GITHUB_ACTION_PATH}" "lifecycle-policy-XXXXXXXX.json")"
 TEMPLATE="${GITHUB_ACTION_PATH}/lifecycle-policy-template.json"
 
-[ -n "${SNAPSHOT_KEEP_DAYS}" ] || SNAPSHOT_KEEP_DAYS="${DEFAULT_SNAPSHOT_KEEP_DAYS}"
-[ -n "${DEVEL_KEEP_DAYS}" ] || DEVEL_KEEP_DAYS="${DEFAULT_DEVEL_KEEP_DAYS}"
+DEFAULT_SNAPSHOT_KEEP_DAYS="30"
+[ -n "${SNAPSHOT_KEEP_DAYS:-}" ] || SNAPSHOT_KEEP_DAYS="${DEFAULT_SNAPSHOT_KEEP_DAYS}"
+
+DEFAULT_DEVEL_KEEP_DAYS="30"
+[ -n "${DEVEL_KEEP_DAYS:-}" ] || DEVEL_KEEP_DAYS="${DEFAULT_DEVEL_KEEP_DAYS}"
 
 CMD=(
 	jq -n
@@ -18,18 +17,24 @@ CMD=(
 		--arg SNAPSHOT_KEEP_STR "Keep only fresh-built devel-* tags that AREN'T the top-level SNAPSHOTs, pushed in the last ${DEVEL_KEEP_DAYS} days"
 )
 
-"${CMD[@]}" "$(<"${TEMPLATE}")" &>"${JSON}" || RC=${?}
+JSON="$(mktemp --tmpdir="${GITHUB_ACTION_PATH}" "generated-lifecycle-policy-XXXXXXXX.json")"
+
 RC=0
+"${CMD[@]}" "$(<"${TEMPLATE}")" &>"${JSON}" || RC=${?}
 if [ ${RC} -ne 0 ] ; then
 	echo "ERROR: Failed to render the lifecycle policy JSON (rc=${RC}): $(<"${JSON}")"
 	exit ${RC}
 fi
 
 echo "Checking the generated JSON syntax..."
-jq -r < "${JSON}" || exit ${?}
+OUT="$(jq -r < "${JSON}" 2>&1)" || RC=${?}
+if [ ${RC} -ne 0 ] ; then
+	echo "ERROR: The rendered JSON has errors (rc=${RC}): ${OUT}\n\n$(<"${JSON}")"
+	exit ${RC}
+fi
 
-echo "Applying the generated lifecycle policy..."
-aws ecr put-lifecycle-policy \
+echo -e "Applying the generated lifecycle policy:\n$(<"${JSON}")"
+exec aws ecr put-lifecycle-policy \
 	--region "${AWS_REGION}" \
 	--repository-name "${IMAGE_URI}" \
 	--lifecycle-policy-text "file://${JSON}"
--- build/permissions.sh
+++ build/permissions.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
 . "${GITHUB_ACTION_PATH}/common.sh"
 
-JSON="$(mktemp --tmpdir="${GITHUB_ACTION_PATH}" "permissions-XXXXXXXX.json")"
 TEMPLATE="${GITHUB_ACTION_PATH}/permissions-template.json"
 
+DEFAULT_AWS_ORG_ID="(unknown)"
+[ -n "${AWS_ORG_ID:-}" ] || AWS_ORG_ID="${DEFAULT_AWS_ORG_ID}"
+
+CMD=(
+	jq -n
+		--arg AWS_ORG_ID "${AWS_ORG_ID}"
+)
+
+JSON="$(mktemp --tmpdir="${GITHUB_ACTION_PATH}" "generated-permissions-XXXXXXXX.json")"
+
 RC=0
-jq -n --arg AWS_ORG_ID "${AWS_ORG_ID}" "$(<"${TEMPLATE}")" &>"${JSON}" || RC=${?}
+"${CMD[@]}" "$(<"${TEMPLATE}")" &>"${JSON}" || RC=${?}
 if [ ${RC} -ne 0 ] ; then
 	echo "ERROR: Failed to render the permissions JSON (rc=${RC}): $(<"${JSON}")"
 	exit ${RC}
 fi
 
-aws ecr set-repository-policy \
+echo "Checking the generated JSON syntax..."
+OUT="$(jq -r < "${JSON}" 2>&1)" || RC=${?}
+if [ ${RC} -ne 0 ] ; then
+	echo "ERROR: The rendered JSON has errors (rc=${RC}): ${OUT}\n\n$(<"${JSON}")"
+	exit ${RC}
+fi
+
+echo -e "Applying the generated repository permissions:\n$(<"${JSON}")"
+exec aws ecr set-repository-policy \
 	--region "${AWS_REGION}" \
 	--repository-name "${IMAGE_URI}" \
 	--policy-text "file://${JSON}"
