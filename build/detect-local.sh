9ffa82cff9a7e9a17c8a913bb584679912a5e666
Diego Rivera <diego.rivera@armedia.com>
2026-04-03 09:18:38 -0600

    Fix local detection to make LOCAL_REGISTRY optional

--- build/detect-local.sh
+++ build/detect-local.sh
@@ -4,15 +4,33 @@
 #
 # Determine if we're doing local development or not
 #
-LOCAL_DEV="false"
-LOCAL_PUBLISH="false"
-if [ -n "${LOCAL_REGISTRY:-}" ] ; then
-	LOCAL_DEV="true"
-	[ -n "${LOCAL_REGISTRY_USERNAME:-}" ] && [ -n "${LOCAL_REGISTRY_PASSWORD:-}" ] && LOCAL_PUBLISH="true"
+if [ -v ACT ] ; then
+	LOCAL_DEV="${ACT,,}"
+elif [ -v LOCAL_DEV ] ; then
+	LOCAL_DEV="${LOCAL_DEV,,}"
+else
+	LOCAL_DEV="false"
 fi
-to_env LOCAL_DEV LOCAL_REGISTRY LOCAL_REGISTRY_USERNAME LOCAL_REGISTRY_PASSWORD LOCAL_PUBLISH
+
+#
+# Sanitize the value!
+#
+case "${LOCAL_DEV}" in
+	true ) ;;
+	* ) LOCAL_DEV="false" ;;
+esac
+to_env LOCAL_DEV
+
+#
+# Only compute these if in local development mode
+#
+if is_local_dev ; then
+	LOCAL_PUBLISH="false"
+	[ -n "${LOCAL_REGISTRY:-}" ] && [ -n "${LOCAL_REGISTRY_USERNAME:-}" ] && [ -n "${LOCAL_REGISTRY_PASSWORD:-}" ] && LOCAL_PUBLISH="true"
+	to_env LOCAL_REGISTRY LOCAL_REGISTRY_USERNAME LOCAL_REGISTRY_PASSWORD LOCAL_PUBLISH
+fi
 
 #
 # Define these early on
 #
-to_env PRIVATE_REGISTRY="${LOCAL_REGISTRY}" PUBLIC_REGISTRY="${LOCAL_REGISTRY}"
+to_env PRIVATE_REGISTRY="${LOCAL_REGISTRY:-}" PUBLIC_REGISTRY="${LOCAL_REGISTRY:-}"
