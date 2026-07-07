f01894d9bfeeac029d59766f32f4cd6f6c473b06
Diego Rivera <diego.rivera@armedia.com>
2026-07-07 11:23:40 -0600

    Fix reports upload - wasn't finding the files

--- build/upload-reports.sh
+++ build/upload-reports.sh
@@ -6,9 +6,10 @@ is_local_dev && MODE="running" || MODE=""
 
 COUNT=0
 while read SRC ; do
-	say "Uploading [${SRC}] to [s3://${REPORTS_BUCKET}/${REPORT_TARGET_PATH}]..."
+	SRC_BASE="${SRC##*/}"
+	say "Uploading [${SRC_BASE}] to [s3://${REPORTS_BUCKET}/${REPORT_TARGET_PATH}]..."
 	# This trick allows us to turn the command into a dud if we so choose (see above!)
-	${MODE} aws s3 cp "${SRC}" "s3://${REPORTS_BUCKET}/${REPORT_TARGET_PATH}/${SRC##*/}"
+	${MODE} aws s3 cp "${SRC}" "s3://${REPORTS_BUCKET}/${REPORT_TARGET_PATH}/${SRC_BASE}"
 	(( ++COUNT ))
-done < <(find . -mindepth 1 -maxdepth 1 -type f -name "${PATTERN}" | sort | cut -c3-)
+done < <(find "${SCAN_TGT_DIR}" -mindepth 1 -maxdepth 1 -type f -name "${TYPE}*" | sort)
 [ ${COUNT} -eq 0 ] && ok "No ${TYPE} reports found to upload!" || ok "Uploaded ${COUNT} ${TYPE} report(s)!"
